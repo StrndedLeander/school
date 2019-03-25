30749ce55f2d19e808956948659967c19856e34a
StrndedLeander <strnded.vandiepen@gmail.com>
2019-03-25 23:04:52 +0100

    first setup and server routes

---
+++ server/src/serverless/students/handler.js
@@ -0,0 +1,17 @@
+'use strict';
+var async = require('async');
+var AWS = require('aws-sdk');
+var fs = require('fs');
+
+module.exports.list = async (event, context) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Go Serverless v1.0! Your function executed successfully!',
+      input: event,
+    }),
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
