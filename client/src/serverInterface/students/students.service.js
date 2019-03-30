2013d505440df87b14e99b3e646fa32c6c1495d9
StrndedLeander <strnded.vandiepen@gmail.com>
2019-03-30 21:44:40 +0100

    modules

---
+++ client/src/serverInterface/students/students.service.js
@@ -0,0 +1,18 @@
+// Object / Class with functions to communicate to vuex after student data came in
+export default {
+  getAllStudents(students) {
+    console.log(students);
+  },
+  getStudent(student) {
+    console.log(student);
+  },
+  createStudent(student) {
+    console.log(student);
+  },
+  updateStudent(student) {
+    console.log(student);
+  },
+  deleteStudent(student) {
+    console.log(student);
+  }
+};
